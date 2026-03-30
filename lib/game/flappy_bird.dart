401ce9386c2768cd54affc791a274f60e21c1e61
codegeass <denzelgatugu@outlook.com>
2026-03-30 23:52:11 +0300

    Add score display component

--- lib/game/flappy_bird.dart
+++ lib/game/flappy_bird.dart
@@ -4,9 +4,11 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_bird/game/game.dart';
+import 'package:flutter/material.dart';
 
 class FlappyBird extends FlameGame with TapCallbacks, HasCollisionDetection {
   late BirdComponent bird;
+  late TextComponent scoreText;
   Timer interval = Timer(GameValues.pipeInterval, repeat: true);
   bool isPipeHit = false;
 
@@ -16,10 +18,27 @@ class FlappyBird extends FlameGame with TapCallbacks, HasCollisionDetection {
       BackgroundComponent(),
       GroundComponent(),
       bird = BirdComponent(),
+      scoreText = highScore,
     ]);
 
     interval.onTick = () => add(PipeGroupComponent());
     return super.onLoad();
+  }
+
+  TextComponent get highScore {
+    return TextComponent(
+      text: 'Score: 0',
+      position: Vector2(size.x / 2, size.y / 2 * 0.2),
+      anchor: .topCenter,
+      textRenderer: TextPaint(
+        style: const TextStyle(
+          fontSize: 40,
+          color: Colors.white,
+          fontFamily: 'Flappy',
+          fontWeight: .bold,
+        ),
+      ),
+    );
   }
 
   @override
